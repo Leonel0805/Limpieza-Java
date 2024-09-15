bb836025730c7400acfc2d845ee956e5de087e1a
= <leoulirodriguez@gmail.com>
2024-09-15 10:41:28 -0300

    add obtenerDatos(): obtener datos de backend user_me

--- cliente/js/user_me.js
+++ cliente/js/user_me.js
@@ -2,10 +2,50 @@ import { cargarHeader } from './utils/generarHeader.js';
 import { isLogin } from './utils/isLogin.js';
 import { sendFormSearchParam } from './utils/sendFormSearchParam.js';
 
+const apiURL = 'http://localhost:8080/api/me'
+const jwt = localStorage.getItem('jwt')
+
+function obtenerDatos(){
+
+    fetch(apiURL, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwt
+        }
+    })
+    .then(response => {
+        
+        if (response.status == 200){
+            return response.json()
+        }
+    })
+    .then(json => {
+
+        cargarDatos(json)
+    })
+    
+}
+
+function cargarDatos(datos){
+
+    let usernameHTML = document.querySelector('#usernameValue')
+    let emailHTML = document.querySelector('#emailValue')
+    let usernameUser = document.querySelector('#usernameUser')
+    let rolUser = document.querySelector('#rolUser')
+
+    rolUser.innerHTML = datos.roles[0].rol_name
+    usernameUser.innerHTML = datos.name
+
+    usernameHTML.innerHTML = datos.name
+    emailHTML.innerHTML = datos.email
+}
+
+
 
 async function init() {
     await cargarHeader();
     isLogin();
+    obtenerDatos()
     sendFormSearchParam();
 }
 
