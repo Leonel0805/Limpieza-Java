cc8d282fe802a9acc9c5d26f9733308116a9011e
= <leoulirodriguez@gmail.com>
2024-10-04 17:31:04 -0300

    update root: update relative root navbar -> absolute root

--- cliente/js/utils/generarHeader.js
+++ cliente/js/utils/generarHeader.js
@@ -2,6 +2,7 @@ export async function cargarHeader(){
 
     let baseURL = window.location.origin
 
+    console.log('base url' + baseURL)
 
     const response = await fetch(baseURL + '/cliente/templates/utils/header.html');
     const data = await response.text();
